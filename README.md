ae049dcf0c5bc2119ec4016af5cdf2140f634167
viktor <mirfaiziev.victor@gmail.com>
2020-12-12 10:28:31 +0100

    add infrastructure (docker) and initial controller

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # comics-collector
+## installation
+1. Run `docker-compose up -d` 
+2. Run `docker-compose exec phpfpm composer install` to install project
+
---
+++ src/Controller/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class IndexController
+ * @package App\Controller
+ */
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", methods={"GET"})
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        return new JsonResponse(['status'=>'OK']);
+    }
+}
