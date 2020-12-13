0f93135dd384bb90b312dbb266fba056282bb5af
viktor <mirfaiziev.victor@gmail.com>
2020-12-13 18:03:48 +0100

    add open api documentation

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Run `docker-compose up -d` 
 2. Run `docker-compose exec phpfpm composer install` to install project
 3. Run `docker-compose exec phpfpm ./bin/phpunit` to run tests
+4. Open `localhost:8080` to see result or `localhost:8080/doc.json` to see documentation
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\DTO\ComicDTO;
 use App\Service\ComicsCollectorService;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +18,16 @@ class IndexController extends AbstractController
 {
     /**
      * @Route("/", methods={"GET"})
+     * @OA\Get(
+     *      operationId="getComics",
+     *      @OA\Response(
+     *          response=200,
+     *          description="get parsed comics",
+     *          @OA\JsonContent(type="array",
+     *               @OA\Items(ref=@Model(type=ComicDTO::class))
+     *          )
+     *     )
+     * )
      * @param ComicsCollectorService $collectorService
      * @return JsonResponse
      */
